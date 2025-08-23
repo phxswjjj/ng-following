c7a3804a7a22e0107f8c3358399fe8d8317d970f
phxswjjj <phxswjjj@gmail.com>
2025-08-23 17:53:31 +0800

    獨立編輯區塊

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,16 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UserCompareService, CompareResultItem, QueryFilterType } from './user-compare.service';
 import { Subscription } from 'rxjs';
+import { EditPanelComponent } from './edit-panel/edit-panel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, EditPanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit, OnDestroy {
-
-  InputUserType = InputUserType;
-  showInputUserType = InputUserType.NA;
-  inputUsersText: string | null = null;
 
   UserSourceType = UserSourceType;
   showUserSourceType = UserSourceType.NA;
@@ -26,7 +23,7 @@ export class AppComponent implements OnInit, OnDestroy {
   combineResult: string[] = [];
   private compareSub?: Subscription;
 
-  constructor(private userCompareService: UserCompareService) {}
+  constructor(private userCompareService: UserCompareService) { }
 
   ngOnInit(): void {
     this.compareSub = this.userCompareService.compareResult$.subscribe(result => {
@@ -49,22 +46,6 @@ export class AppComponent implements OnInit, OnDestroy {
   copyToClipboard(resultTextarea: HTMLTextAreaElement): void {
     resultTextarea.select();
     document.execCommand('copy');
-    resultTextarea.setSelectionRange(0, 0); // 可選，取消選取
-  }
-
-  submitInputUsers(): void {
-    const inputUsers = (this.inputUsersText ?? '').split('\n').map(line => line.trim()).filter(line => line.length > 0);
-
-    if (this.showInputUserType === InputUserType.Origin) {
-      this.userCompareService.appendToOriginalList(inputUsers);
-    } else if (this.showInputUserType === InputUserType.New) {
-      this.userCompareService.appendToNewList(inputUsers);
-    }
-    //reset input
-    this.inputUsersText = null;
-    this.showInputUserType = InputUserType.NA;
-
-    this.userCompareService.compareResults();
   }
 
   filterCompareResult(): void {
@@ -90,12 +71,6 @@ export class AppComponent implements OnInit, OnDestroy {
         break;
     }
   }
-}
-
-export enum InputUserType {
-  NA,
-  Origin,
-  New,
 }
 
 export enum UserSourceType {
---
+++ src/app/edit-panel/edit-panel.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CompareResultItem, UserCompareService } from '../user-compare.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-panel',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './edit-panel.component.html',
+  styleUrl: './edit-panel.component.css'
+})
+export class EditPanelComponent {
+
+  InputUserType = InputUserType;
+  showInputUserType = InputUserType.NA;
+  inputUsersText: string | null = null;
+
+  compareResult: CompareResultItem[] | null = null;
+  combineResult: string[] = [];
+  private compareSub?: Subscription;
+
+  constructor(private userCompareService: UserCompareService) { }
+
+  ngOnInit(): void {
+    this.compareSub = this.userCompareService.compareResult$.subscribe(result => {
+      this.compareResult = result;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.compareSub?.unsubscribe();
+  }
+
+  submitInputUsers(): void {
+    const inputUsers = (this.inputUsersText ?? '').split('\n').map(line => line.trim()).filter(line => line.length > 0);
+
+    if (this.showInputUserType === InputUserType.Origin) {
+      this.userCompareService.appendToOriginalList(inputUsers);
+    } else if (this.showInputUserType === InputUserType.New) {
+      this.userCompareService.appendToNewList(inputUsers);
+    }
+    //reset input
+    this.inputUsersText = null;
+    this.showInputUserType = InputUserType.NA;
+
+    this.userCompareService.compareResults();
+  }
+
+}
+
+export enum InputUserType {
+  NA,
+  Origin,
+  New,
+}
